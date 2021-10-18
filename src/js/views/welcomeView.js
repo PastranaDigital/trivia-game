d8362129e636292e72e48f1a549968feb7d7ff61
PastranaDigital <53458320+PastranaDigital@users.noreply.github.com>
2021-10-18 10:31:37 -0500

    still getting front page organized

--- src/js/views/welcomeView.js
+++ src/js/views/welcomeView.js
@@ -10,11 +10,64 @@ class WelcomeView extends View {
 		window.addEventListener('load', (e) => handler());
 	}
 
+	addHandlerClick(handler) {
+		this._parentElement.addEventListener('click', function (e) {
+			const btn = e.target.closest('.btn--inline'); //? search up the DOM
+			if (!btn) return;
+			const goToPage = +btn.dataset.goto;
+			handler(goToPage);
+		});
+	}
+
 	_generateMarkup() {
 		return `
-			<p>Score: ${this._data.correctAnswers * 100}</p>
-			<p>Correct: ${this._data.correctAnswers}</p>
-			<p>Total: ${this._data.totalSelectedQuestions}</p>
+			<div id="overlay" class="active">
+				<div id="title">
+					<h1>Trivia Game</h1>
+				</div>
+			</div>
+			<div class="modal active" id="modal">
+				<div id="questionamt" class="modal-body">
+					<h1>How many questions for this game?</h1>
+
+					<!-- https://codepen.io/viestursm/pen/mdJeKVw -->
+					<form action="javascript:void(0);">
+						<div class="number-input-container">
+							<button
+								type="button"
+								class="number-button button-decrement"
+								onclick="setNumber()"
+								data-input-id="questions"
+								data-operation="decrement"
+							></button>
+							<div class="number-input">
+								<input
+									type="number"
+									id="questions"
+									name="questions"
+									class="number-input-text-box"
+									value="10"
+									min="5"
+									max="100"
+									oninput="handleNumberInput()"
+									onblur="handleNumberInputBlur(event)"
+									data-step="5"
+								/>
+							</div>
+							<button
+								type="button"
+								class="number-button button-increment"
+								onclick="setNumber()"
+								data-input-id="questions"
+								data-operation="increment"
+							></button>
+						</div>
+					</form>
+				</div>
+				<div class="modal-footer">
+					<button data-start-button class="start-button">GO</button>
+				</div>
+			</div>
 		`;
 	}
 }
